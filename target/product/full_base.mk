0bc3f490d006e1f260562bfe8f105ee73bdff31f
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-03-04 04:25:11 +0700

    product: Put in_ID first in the list, so make it default.

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -47,8 +47,8 @@ PRODUCT_VENDOR_PROPERTIES := \
     ro.config.ringtone?=Ring_Synth_04.ogg \
     ro.config.notification_sound?=pixiedust.ogg
 
-# Put en_US first in the list, so make it default.
-PRODUCT_LOCALES := en_US
+# Put in_ID first in the list, so make it default.
+PRODUCT_LOCALES := in_ID
 
 # Get some sounds
 ifeq ($(LMODROID_BUILD),)
